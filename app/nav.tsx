a1755ffdaed6a64c23c2acbcbe3128d75ed76eb0
Tadeáš Kótka <tadekotk@gmail.com>
2026-01-21 14:11:06 +0100

    trochu změna navbaru

--- app/nav.tsx
+++ app/nav.tsx
@@ -1,18 +1,18 @@
 export function Nav() {
     return (
-        <nav className="flex justify-center fixed top-0 w-full p-4 z-50 backdrop-blur-xl shadow-sm shadow-var(--foreground)">
+        <nav className="flex justify-center fixed top-0 w-full p-4 z-50 backdrop-blur-xl shadow-sm shadow-var(--foreground">
             <div>
-                <a href="#home" className="text-var(--foreground) border-r border-var(--foreground) p-2 transition-all duration-300 hover:shadow-black [text-shadow:0_0_0.5px_rgba(0,0,0,0)] hover:[text-shadow:0.5px_0_0_currentColor]">Úvod</a>
+                <a href="#home" className="text-var(--foreground) border-r border-var(--foreground) p-2 transition-all duration-300 hover:shadow-black [text-shadow:0_0_0.5px_rgba(0,0,0,0)] hover:[text-shadow:0.5px_0_0_currentColor] pr-4 pl-0">Úvod</a>
             </div>
             <div>
-                <a href="#services" className="text-var(--foreground) border-r border-var(--foreground) p-2 transition-all duration-300 hover:shadow-black [text-shadow:0_0_0.5px_rgba(0,0,0,0)] hover:[text-shadow:0.5px_0_0_currentColor]">Služby</a>
+                <a href="#services" className="text-var(--foreground) border-r border-var(--foreground) p-2 transition-all duration-300 hover:shadow-black [text-shadow:0_0_0.5px_rgba(0,0,0,0)] hover:[text-shadow:0.5px_0_0_currentColor] pl-4 pr-4">Služby</a>
             </div>
             <div>
-                <a href="#about" className="text-var(--foreground) border-r border-var(--foreground) p-2 transition-all duration-300 hover:shadow-black [text-shadow:0_0_0.5px_rgba(0,0,0,0)] hover:[text-shadow:0.5px_0_0_currentColor]">O mně</a>
+                <a href="#about" className="text-var(--foreground) border-r border-var(--foreground) p-2 transition-all duration-300 hover:shadow-black [text-shadow:0_0_0.5px_rgba(0,0,0,0)] hover:[text-shadow:0.5px_0_0_currentColor] pl-4 pr-4">O mně</a>
             </div>
 
             <div>
-                <a href="#contact" className="text-var(--foreground) p-2 transition-all duration-300 hover:shadow-black [text-shadow:0_0_0.5px_rgba(0,0,0,0)] hover:[text-shadow:0.5px_0_0_currentColor]">Kontakt</a>
+                <a href="#contact" className="text-var(--foreground) p-2 transition-all duration-300 hover:shadow-black [text-shadow:0_0_0.5px_rgba(0,0,0,0)] hover:[text-shadow:0.5px_0_0_currentColor] pl-4 pr-0">Kontakt</a>
             </div>
         </nav>
     );
