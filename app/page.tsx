4622493072e4f2b6b0934b17c938fffdbae728e4
Tadeáš Kótka <tadekotk@gmail.com>
2026-01-21 11:55:33 +0100

    Dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -16,27 +16,28 @@ export default function Home() {
   return (
     <div>
       <Nav />
+      <div id="bg-div" className="fixed top-0 left-0 w-full h-full z-5 object-cover"></div>
       <div className="h-16"></div>
       <video src="/bg.mp4" ref={videoRef} autoPlay loop muted className="fixed top-0 left-0 w-full h-full z-0 object-cover" />
       <main className="relative flex flex-col items-center z-10">
 
         <section id="home" className="flex flex-col items-center py-5 w-[60%] scroll-mt-16">
-          <h1 className="border-b border-black w-[60%] text-center text-2xl mb-5">Úvod</h1>
+          <h1 className="border-b border-var(--foreground) w-[60%] text-center text-2xl mb-5">Úvod</h1>
           <p className="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem laudantium quo deserunt dolore doloribus? Non accusamus voluptatem ut provident soluta. Minima ducimus vero inventore neque, ullam dolor nesciunt esse expedita!</p>
         </section>
 
         <section id="services" className="flex flex-col items-center py-5 w-[60%] scroll-mt-16">
-          <h1 className="border-b border-black w-[60%] text-center text-2xl mb-5">Služby</h1>
+          <h1 className="border-b border-var(--foreground) w-[60%] text-center text-2xl mb-5">Služby</h1>
           <p className="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem laudantium quo deserunt dolore doloribus? Non accusamus voluptatem ut provident soluta. Minima ducimus vero inventore neque, ullam dolor nesciunt esse expedita!</p>
         </section>
 
         <section id="about" className="flex flex-col items-center py-5 w-[60%] scroll-mt-16">
-          <h1 className="border-b border-black w-[60%] text-center text-2xl mb-5">O mně</h1>
+          <h1 className="border-b border-var(--foreground) w-[60%] text-center text-2xl mb-5">O mně</h1>
           <p className="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem laudantium quo deserunt dolore doloribus? Non accusamus voluptatem ut provident soluta. Minima ducimus vero inventore neque, ullam dolor nesciunt esse expedita!</p>
         </section>
 
         <section id="contact" className="flex flex-col items-center py-5 w-[60%] scroll-mt-16">
-          <h1 className="border-b border-black w-[60%] text-center text-2xl mb-5">Kontakt</h1>
+          <h1 className="border-b border-var(--foreground) w-[60%] text-center text-2xl mb-5">Kontakt</h1>
           <p className="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem laudantium quo deserunt dolore doloribus? Non accusamus voluptatem ut provident soluta. Minima ducimus vero inventore neque, ullam dolor nesciunt esse expedita!</p>
         </section>
 
